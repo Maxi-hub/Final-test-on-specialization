6ea69be6d01e6ff87f8aa048062475c5d20100f2
Emma <em.lebedeva85@gmail.com>
2024-02-22 22:27:11 +0300

    fixed counting of animals and writing to a file

---
+++ Animals/src/AnimalManager.java
@@ -0,0 +1,153 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class AnimalManager {
+    private int catCount = 0; // listOfCats().size();
+    private int dogCount = 0; // listOfDogs().size();
+    private int hamsterCount = 0; // listOfHamsters().size();
+    
+
+    public ArrayList<Animal> listOfCats() {
+        ArrayList<String> catCommands = new ArrayList<>();
+        catCommands.add("play, run");
+        ArrayList<Animal> cats = new ArrayList<Animal>();
+        Animal newCat_1 = new Cats("cat", "Suzy", "2020-11-11", catCommands);
+        Animal newCat_2 = new Cats("cat", "Tom", "2022-11-11", catCommands);
+        Animal newCat_3 = new Cats("cat", "Jary", "2019-11-11", catCommands);
+        cats.add(newCat_1);
+        cats.add(newCat_2);
+        cats.add(newCat_3);
+        return cats;
+    }
+
+    public ArrayList<Animal> listOfDogs() {
+        ArrayList<String> dogCommands = new ArrayList<>();
+        dogCommands.add("bark, seat");
+        ArrayList<Animal> dogs = new ArrayList<Animal>();
+        Animal newDog_1 = new Dogs("dog", "Tom", "2002-01-01", dogCommands);
+        Animal newDog_2 = new Dogs("dog", "Bobby", "2002-02-02", dogCommands);
+        Animal newDog_3 = new Dogs("dog", "Rex", "2002-03-03", dogCommands);
+        dogs.add(newDog_1);
+        dogs.add(newDog_2);
+        dogs.add(newDog_3);
+        return dogs;
+    }
+
+    public ArrayList<Animal> listOfHamsters() {
+        ArrayList<String> hamsterCommands = new ArrayList<>();
+        hamsterCommands.add("sleep, eat");
+        ArrayList<Animal> hamsters = new ArrayList<>();
+        Animal newHamster_1 = new Hamsters("hamster", "Bob", "2003-01-01", hamsterCommands);
+        Animal newHamster_2 = new Hamsters("hamster", "Sam", "2003-02-02", hamsterCommands);
+        Animal newHamster_3 = new Hamsters("hamster", "Lory", "2003-03-03", hamsterCommands);
+        hamsters.add(newHamster_1);
+        hamsters.add(newHamster_2);
+        hamsters.add(newHamster_3);
+        return hamsters;
+    }
+
+    public static File CreateFile() throws IOException {
+        File fileName = new File("Registry");
+        if (!fileName.exists()) {
+            fileName.createNewFile();
+        }
+        return fileName;
+    }
+
+    public void WriteToFileMainAnimals(File fileName, AnimalRegistry animalRegistry) {
+        try {
+            BufferedWriter br = new BufferedWriter(new FileWriter(fileName));
+            for (int i = 0; i < animalRegistry.animals.size(); i++) {
+                br.write(animalRegistry.animals.get(i).toString() + "\n");
+            }
+            br.write("Count of cats: " + catCount);
+            System.out.println("Cats: " + catCount);
+            br.newLine();
+            br.write("Count of dogs: " + dogCount);
+            System.out.println("Dogs: " + dogCount);
+            br.newLine();
+            br.write("Count of hamsters: " + hamsterCount);
+            System.out.println("Hamsters: " + hamsterCount);
+            br.newLine();
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void ReadFromFile(File fileName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Обработка данных: увеличение счетчиков для каждого типа животных
+                if (line.contains("Cat")) {
+                    catCount++;
+                } else if (line.contains("Dog")) {
+                    dogCount++;
+                } else if (line.contains("Hamster")) {
+                    hamsterCount++;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addAnimal(File fileName, String type, Animal newAnimal) {
+        try {
+            BufferedWriter br = new BufferedWriter(new FileWriter(fileName, true));
+            br.newLine();
+            newAnimal.addNewCommand();
+            newAnimal.printCommands();
+            System.out.println("\n" + newAnimal + "\n");
+            if (type.equals("cat")) {
+                catCount++;
+            } else if (type.equals("dog")) {
+                dogCount++;
+            } else if (type.equals("hamster")) {
+                hamsterCount++;
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void WriteToFile(File fileName, AnimalRegistry animalRegistry, Animal newAnimal) {
+        try {
+            BufferedWriter br = new BufferedWriter(new FileWriter(fileName, true));
+            int totalAnimals = (catCount + dogCount + hamsterCount);
+            animalRegistry.addNewAnimal(newAnimal);
+            br.append(newAnimal.toString() + "\n");
+            br.append("Count of cats: " + catCount);
+            System.out.println("Cats: " + catCount);
+            br.newLine();
+            br.append("Count of dogs: " + dogCount);
+            System.out.println("Dogs: " + dogCount);
+            br.newLine();
+            br.append("Count of hamsters: " + hamsterCount);
+            System.out.println("Hamsters: " + hamsterCount);
+            br.newLine();
+            System.out.println(
+                    "\n" + "Total number of animals in the registry: " + totalAnimals + "\n");
+            animalRegistry.printAllAnimal();
+            animalRegistry.sortAnimalsByBirthday();
+            System.out.println("\n" + "List of animals sorted by birthday: ");
+            for (Animal animal : animalRegistry.animals) {
+                System.out.println(animal);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- Animals/src/Main.java
+++ Animals/src/Main.java
@@ -1,105 +1,36 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
-        AnimalRegistry animalRegistry = new AnimalRegistry();
-
+       
+        File newFile = AnimalManager.CreateFile();
         Animal newAnimal = Pets.getNewAnimal();
-        newAnimal.addNewCommand();
-        newAnimal.printCommands();
-        System.out.println("\n" + newAnimal + "\n");
-
-        ArrayList<String> catCommands = new ArrayList<>();
-        catCommands.add("bark, stay");
-        ArrayList<Animal> cats = new ArrayList<Animal>();
-        Animal newCat_1 = new Cats("cat", "Suzy", "2020-11-11", catCommands);
-        Animal newCat_2 = new Cats("cat", "Tom", "2022-11-11", catCommands);
-        Animal newCat_3 = new Cats("cat", "Jary", "2019-11-11", catCommands);
-        cats.add(newCat_1);
-        cats.add(newCat_2);
-        cats.add(newCat_3);
-        animalRegistry.addNewAnimal(newCat_1);
-        animalRegistry.addNewAnimal(newCat_2);
-        animalRegistry.addNewAnimal(newCat_3);
-
-        ArrayList<String> dogCommands = new ArrayList<>();
-        dogCommands.add("bark, stay");
-        ArrayList<Animal> dogs = new ArrayList<Animal>();
-        Animal newDog_1 = new Dogs("dog", "Tom", "2002-01-01", dogCommands);
-        Animal newDog_2 = new Dogs("dog", "Bobby", "2002-02-02", dogCommands);
-        Animal newDog_3 = new Dogs("dog", "Rex", "2002-03-03", dogCommands);
-        dogs.add(newDog_1);
-        dogs.add(newDog_2);
-        dogs.add(newDog_3);
-        animalRegistry.addNewAnimal(newDog_1);
-        animalRegistry.addNewAnimal(newDog_2);
-        animalRegistry.addNewAnimal(newDog_3);
-
-        ArrayList<String> hamsterCommands = new ArrayList<>();
-        hamsterCommands.add("sleep, eat");
-        ArrayList<Animal> hamsters = new ArrayList<>();
-        Animal newHamster_1 = new Hamsters("hamster", "Bob", "2003-01-01", hamsterCommands);
-        Animal newHamster_2 = new Hamsters("hamster", "Sam", "2003-02-02", hamsterCommands);
-        Animal newHamster_3 = new Hamsters("hamster", "Lory", "2003-03-03", hamsterCommands);
-        hamsters.add(newHamster_1);
-        hamsters.add(newHamster_2);
-        hamsters.add(newHamster_3);
-        animalRegistry.addNewAnimal(newHamster_1);
-        animalRegistry.addNewAnimal(newHamster_2);
-        animalRegistry.addNewAnimal(newHamster_3);
-
-        if (newAnimal.getType().equals("cat")) {
-            cats.add(newAnimal);
-        } else if (newAnimal.getType().equals("dog")) {
-            dogs.add(newAnimal);
-        } else {
-            hamsters.add(newAnimal);
+        AnimalRegistry animalRegistry = new AnimalRegistry();
+        AnimalManager animalManager = new AnimalManager();
+        
+        
+        ArrayList<Animal> cats = animalManager.listOfCats();
+        for (int i = 0; i < cats.size(); i++) {
+            animalRegistry.addNewAnimal(cats.get(i));
         }
 
-        try {
-            File fileName = new File("Registry");
-            if (!fileName.exists()) {
-                fileName.createNewFile();
-            }
-
-            PrintWriter pw = new PrintWriter(new FileWriter(fileName, true));
-            pw.append(animalRegistry.animals.toString() + "\n");
-            animalRegistry.addNewAnimal(newAnimal);
-            pw.append(newAnimal.toString() + "\n");
-            animalRegistry.printAllAnimal();
-            animalRegistry.sortAnimalsByBirthday();
-            System.out.println("\n" + "List of animals sorted by birthday:");
-            for (Animal animal : animalRegistry.animals) {
-                System.out.println(animal);
-            }
-            int catCount = animalRegistry.countAnimalByType("cat");
-            System.out.println("Count of cats: " + catCount);
-            int dogCount = animalRegistry.countAnimalByType("dog");
-            System.out.println("Count of dogs: " + dogCount);
-            int hamsterCount = animalRegistry.countAnimalByType("hamster");
-            System.out.println("Count of hamsters: " + hamsterCount);
-            System.out
-                    .println("\n" + "Total number of animals in the registry: " + animalRegistry.getTotalAnimals()
-                            + "\n");
-            pw.close();
-
-            // BufferedReader br = new BufferedReader(new FileReader(fileName));
-            // String line;
-            // while ((line = br.readLine()) != null) {
-            //     System.out.println(line);
-            // }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        ArrayList<Animal> dogs = animalManager.listOfDogs();
+        for (int i = 0; i < dogs.size(); i++) {
+            animalRegistry.addNewAnimal(dogs.get(i));
         }
 
+        ArrayList<Animal> hamsters = animalManager.listOfHamsters();
+        for (int i = 0; i < hamsters.size(); i++) {
+            animalRegistry.addNewAnimal(hamsters.get(i));
+        }
+        
+        // animalManager.WriteToFileMainAnimals(newFile, animalRegistry);
+        animalManager.ReadFromFile(newFile);
+        animalManager.addAnimal(newFile, newAnimal.getType(), newAnimal);
+        animalManager.WriteToFile(newFile, animalRegistry, newAnimal);
+        
+             
     }
 }
